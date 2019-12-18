13690c7123779c428c32ecb5146d56ce202b1bf9
valery.buka <valpal314@gmail.com>
2019-12-18 15:26:33 +0300

    create basic react application template

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const App = props => (
     <div>
-      <h1>Application components will be here</h1>
+        <h1>Application components will be here</h1>
+        <hr/>
     </div>
 )
 
