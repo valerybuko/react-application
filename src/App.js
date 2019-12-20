1035303c11af504c01471cd783022827ec77f4fe
valery.buka <valpal314@gmail.com>
2019-12-20 17:08:52 +0300

    add @font-face

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import Title from "./Components/Title";
 
 const App = () => {
-    return <div/>;
+    return <div><Title>Tree for IT</Title></div>;
 };
 
 export default App;
--- src/Components/Title/styles.js
+++ src/Components/Title/styles.js
@@ -1,11 +1,7 @@
 import {css} from 'styled-components';
-import colors from "../../themes/colors";
-import fonts from "../../themes/fonts";
+import {decorativeFonts, defaultFonts} from "../../themes/fonts";
 
 export const TitleStyles = css`
-    font-family: ${fonts.fontFamilyDefaultTitle};
-    font-size: ${props => props.fontSize || fonts.fontSize24}
-    font-style: ${fonts.fontStyleNormal};
-    font-weight: ${fonts.fontWeightBold};
-    color: ${colors.black}
+    font-family: ${decorativeFonts.fontFamily};
+    font-size: ${props => props.fontSize || defaultFonts.titleFontSize}
 `;
