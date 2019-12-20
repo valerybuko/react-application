1f6706adb03690187c000863c66b124778486ea9
valery.buka <valpal314@gmail.com>
2019-12-20 10:03:55 +0300

    return empty basic container

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Button from "./Components/Button";
-import {noop} from "./utils/noop";
 
 const App = () => {
-    return <Button onClick={noop}>Your Workplaces</Button>;
+    return <div></div>;
 }
 
 export default App;
