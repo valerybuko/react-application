e75107f87e3ffeff5fb48d26da518c375df06ee6
valery.buka <valpal314@gmail.com>
2019-12-20 10:03:10 +0300

    show button

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Button from "./Components/Button";
 import {noop} from "./utils/noop";
 
 const App = () => {
-    return <Button onClick={noop}>Your Workplaces</Button>
+    return <Button onClick={noop}>Your Workplaces</Button>;
 }
 
 export default App;
