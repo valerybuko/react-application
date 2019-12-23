0f3d622370310148babc3cf4b9b5e92bb7f11be6
valery.buka <valpal314@gmail.com>
2019-12-23 13:34:49 +0300

    test Image component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import {imagesURL} from "./mockdata/imagesURL";
+import Image from "./Components/Image";
+
+const images = imagesURL.map(image => <Image key={image.id} src={image.url} alt={image.alt} title={image.title}/>);
 
 const App = () => {
-    return <div/>;
+    return <div>
+        {images}
+    </div>;
 };
 
 export default App;
