5ec2c7c67a203b5990c77c2b498063e55b739177
valerybuko <valpal314@gmail.com>
2019-12-29 18:37:54 +0300

    fix imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-// eslint-disable-next-line import/no-unresolved
 import Layout from './Components/Layout';
 
 const App = () => (
