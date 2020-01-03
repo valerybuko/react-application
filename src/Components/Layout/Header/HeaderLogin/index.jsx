5eabf3afc388b6d45ea5bc6290a6c5d5e9b8cc4d
valery.buka <valpal314@gmail.com>
2020-01-03 17:18:17 +0300

    fix sign in link styles

--- src/Components/Layout/Header/HeaderLogin/index.jsx
+++ src/Components/Layout/Header/HeaderLogin/index.jsx
@@ -3,13 +3,31 @@ import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 import { HeaderLoginComponentStyles } from './style';
 import Button from '../../../Button';
-import LinkItem from '../../../LinkItem';
+import colors from '../../../../themes/colors';
 
 const HeaderLoginComponent = styled.div(HeaderLoginComponentStyles);
 
+const LoginLinkItem = styled.a`
+    color: ${colors.black};
+    text-decoration: none;
+    display: inline-block;
+    margin: 5px;
+    padding: 0;
+     
+    &:hover {
+        color: ${colors.lilac};
+        text-decoration: none; 
+        cursor: pointer;
+    }
+    
+    &:active {
+        border: none;
+    }
+`;
+
 const HeaderLogin = () => (
     <HeaderLoginComponent>
-        <NavLink to='/sign-in'><LinkItem>Sign In</LinkItem></NavLink>
+        <NavLink to='/sign-in'><LoginLinkItem>Sign In</LoginLinkItem></NavLink>
         <NavLink to='login'><Button>GET STARTED</Button></NavLink>
     </HeaderLoginComponent>
 );
