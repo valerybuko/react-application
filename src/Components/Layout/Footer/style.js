3d667e46860ac681e37a0a8843844eb767a75c20
valerybuko <valpal314@gmail.com>
2020-01-06 20:45:06 +0300

    set footer width

--- src/Components/Layout/Footer/style.js
+++ src/Components/Layout/Footer/style.js
@@ -5,7 +5,7 @@ export const FooterStyles = css`
     display: flex;
     flex: 0 0 5em;
     flex-direction: column;
-    width: 1260px;
+    width: 100%;
     margin: 0 auto;
     background-color: ${colors.whiteDarker};
     border: 1px solid darkblue;
