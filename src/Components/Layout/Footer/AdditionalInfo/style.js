125463663aaad801287efde6dc3877a5e49feaeb
valerybuko <valpal314@gmail.com>
2020-01-07 11:57:28 +0300

    add media queries to contact info

--- src/Components/Layout/Footer/AdditionalInfo/style.js
+++ src/Components/Layout/Footer/AdditionalInfo/style.js
@@ -5,4 +5,9 @@ export const AdditionalInfoStyles = css`
     flex-flow: row nowrap;
     justify-content: space-between;
     border: 1px solid gray;
+    
+    @media (max-width: 750px) {
+        flex-direction: column;
+        align-items: center;
+    }
 `;
--- src/Components/Layout/Footer/TopInfo/FooterLinksListContainer/index.jsx
+++ src/Components/Layout/Footer/TopInfo/FooterLinksListContainer/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { FooterLinkListContainerStyles } from './style';
+import {FooterLinkListContainerStyles} from './style';
 import FooterLinkList from './FooterLinkList';
 import LinkItem from '../../../../LinkItem';
 import colors from '../../../../../themes/colors';
@@ -32,32 +32,51 @@ const FooterTitle = styled.div`
 
 const FooterLinkListContainerComponent = styled.div`${FooterLinkListContainerStyles}`;
 
+const FooterLinkListSection = styled.div`
+    display: flex;
+    flex-direction: row;
+    
+    @media (max-width: 750px) {
+        flex-direction: column;
+    }
+`;
+
 const FooterLinkListContainer = (props) => (
     <FooterLinkListContainerComponent>
-        <FooterLinkList>
-           <FooterTitle fontSize={'14px'}>PRODUCT</FooterTitle>
-            <li><LinkItemFooter href={'https://slack.com/intl/en-by/features'}>Why slack</LinkItemFooter></li>
-            <li><LinkItemFooter href={'https://slack.com/intl/en-by/enterprise'}>Security</LinkItemFooter></li>
-            <li><LinkItemFooter href={'https://slack.com/intl/en-by/customer-stories'}>Customer Stories</LinkItemFooter></li>
-        </FooterLinkList>
-        <FooterLinkList>
-            <FooterTitle fontSize={'14px'}>SLACK FOR TEAMS</FooterTitle>
-            <li><LinkItemFooter href={'https://slack.com/intl/en-by/solutions/engineering'}>Engineering</LinkItemFooter></li>
-            <li><LinkItemFooter href={'https://slack.com/intl/en-by/solutions/sales'}>Sales</LinkItemFooter></li>
-            <li><LinkItemFooter href={'https://slack.com/intl/en-by/solutions/human-resources'}>Human Resources</LinkItemFooter></li>
-        </FooterLinkList>
-        <FooterLinkList>
-            <FooterTitle fontSize={'14px'}>RESOURCES</FooterTitle>
-            <li><LinkItemFooter href={'https://slack.com/intl/en-by/slack-tips'}>Slack Tips</LinkItemFooter></li>
-            <li><LinkItemFooter href={'https://api.slack.com/'}>API</LinkItemFooter></li>
-            <li><LinkItemFooter href={'https://slack.com/intl/en-by/downloads/windows'}>Download Slack</LinkItemFooter></li>
-        </FooterLinkList>
-        <FooterLinkList>
-            <FooterTitle fontSize={'14px'}>COMPANY</FooterTitle>
-            <li><LinkItemFooter href={'https://slack.com/intl/en-by/about'}>About Us</LinkItemFooter></li>
-            <li><LinkItemFooter href={'https://slack.com/intl/en-by/newsroom'}>News</LinkItemFooter></li>
-            <li><LinkItemFooter href={'https://slack.com/intl/en-by/careers'}>Careers</LinkItemFooter></li>
-        </FooterLinkList>
+        <FooterLinkListSection>
+            <FooterLinkList>
+                <FooterTitle fontSize={'14px'}>PRODUCT</FooterTitle>
+                <li><LinkItemFooter href={'https://slack.com/intl/en-by/features'}>Why slack</LinkItemFooter></li>
+                <li><LinkItemFooter href={'https://slack.com/intl/en-by/enterprise'}>Security</LinkItemFooter></li>
+                <li><LinkItemFooter href={'https://slack.com/intl/en-by/customer-stories'}>Customer
+                    Stories</LinkItemFooter>
+                </li>
+            </FooterLinkList>
+            <FooterLinkList>
+                <FooterTitle fontSize={'14px'}>SLACK FOR TEAMS</FooterTitle>
+                <li><LinkItemFooter
+                    href={'https://slack.com/intl/en-by/solutions/engineering'}>Engineering</LinkItemFooter></li>
+                <li><LinkItemFooter href={'https://slack.com/intl/en-by/solutions/sales'}>Sales</LinkItemFooter></li>
+                <li><LinkItemFooter href={'https://slack.com/intl/en-by/solutions/human-resources'}>Human
+                    Resources</LinkItemFooter></li>
+            </FooterLinkList>
+        </FooterLinkListSection>
+        <FooterLinkListSection>
+            <FooterLinkList>
+                <FooterTitle fontSize={'14px'}>RESOURCES</FooterTitle>
+                <li><LinkItemFooter href={'https://slack.com/intl/en-by/slack-tips'}>Slack Tips</LinkItemFooter></li>
+                <li><LinkItemFooter href={'https://api.slack.com/'}>API</LinkItemFooter></li>
+                <li><LinkItemFooter href={'https://slack.com/intl/en-by/downloads/windows'}>Download
+                    Slack</LinkItemFooter>
+                </li>
+            </FooterLinkList>
+            <FooterLinkList>
+                <FooterTitle fontSize={'14px'}>COMPANY</FooterTitle>
+                <li><LinkItemFooter href={'https://slack.com/intl/en-by/about'}>About Us</LinkItemFooter></li>
+                <li><LinkItemFooter href={'https://slack.com/intl/en-by/newsroom'}>News</LinkItemFooter></li>
+                <li><LinkItemFooter href={'https://slack.com/intl/en-by/careers'}>Careers</LinkItemFooter></li>
+            </FooterLinkList>
+        </FooterLinkListSection>
     </FooterLinkListContainerComponent>
 );
 
