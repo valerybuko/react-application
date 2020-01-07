56c45b367a61802f39ae3e2e51ee7ecfd091a135
valerybuko <valpal314@gmail.com>
2020-01-07 12:22:26 +0300

    set footer font-size from the theme

--- src/Components/Layout/Footer/AdditionalInfo/style.js
+++ src/Components/Layout/Footer/AdditionalInfo/style.js
@@ -1,9 +1,11 @@
 import { css } from 'styled-components';
+import { defaultFonts } from '../../../../themes/fonts';
 
 export const AdditionalInfoStyles = css`
     display: flex;
     flex-flow: row nowrap;
     justify-content: space-between;
+    font-size: ${defaultFonts.footerFontSize}
     border: 1px solid gray;
     
     @media (max-width: 750px) {
--- src/Components/Layout/Footer/TopInfo/FooterLinksListContainer/style.js
+++ src/Components/Layout/Footer/TopInfo/FooterLinksListContainer/style.js
@@ -1,10 +1,12 @@
 import { css } from 'styled-components';
+import { defaultFonts } from '../../../../../themes/fonts';
 
 export const FooterLinkListContainerStyles = css`
     flex: 3;
     display: flex;
     justify-content: center;
     padding: 10px;
+    font-size: ${defaultFonts.footerFontSize}
     border: 1px solid red;
     
     @media (max-width: 1005px) {
