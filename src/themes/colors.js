dc491cbf4e58c78675160ebc416318551d3354a9
valery.buka <valpal314@gmail.com>
2020-01-08 10:44:16 +0300

    add colors to the theme

--- src/themes/colors.js
+++ src/themes/colors.js
@@ -1,9 +1,11 @@
 export default {
   lilac: '#611f69',
   white: '#fff',
+  whiteSmoke: '#f5f5f5;',
   black: '#000',
   darkblue: '#1264a3',
+  darkblue_experssed: '#1264a3',
   gray: '#696969',
   darkgray: '#454545',
-  whiteSmoke: '#f5f5f5;',
+  lightlyGray: '#D8D8D8',
 };
