4266f12f80a0c8ba66c0d11dfeade544a5f4faf5
valery.buka <valpal314@gmail.com>
2020-01-10 10:52:09 +0300

    add center align to the login form

--- src/Components/LoginForm/index.jsx
+++ src/Components/LoginForm/index.jsx
@@ -1,26 +1,33 @@
-import React from "react";
-import TextField from "./TextField";
+import React from 'react';
+import styled from 'styled-components';
 import { makeStyles } from '@material-ui/core/styles';
-import Button from "../Button";
+import TextField from './TextField';
+import Button from '../Button';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        '& > *': {
-            margin: theme.spacing(1),
-            width: 300,
-        },
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: 300,
     },
+  },
 }));
 
+const LoginFormWrapper = styled.div`
+    margin: 0 auto;
+`;
+
 const LoginForm = (props) => {
-    const classes = useStyles();
-    return (
-        <form className={classes.root}>
-            <TextField type={'email'} id="outlined-basic" label="Email" variant="outlined" required/><br/>
-            <TextField type={'password'} id="outlined-basic" label="Password" variant="outlined" required/><br/>
-            <Button>Log In</Button>
-        </form>
-    );
+  const classes = useStyles();
+  return (
+      <LoginFormWrapper>
+          <form className={classes.root}>
+              <TextField type={'email'} id="outlined-basic" label="Email" variant="outlined" required/><br/>
+              <TextField type={'password'} id="outlined-basic" label="Password" variant="outlined" required/><br/>
+              <Button>Log In</Button>
+          </form>
+      </LoginFormWrapper>
+  );
 };
 
 export default LoginForm;
