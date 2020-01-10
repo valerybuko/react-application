a7e7d1182e1ec1c83eebc5b6ff9864e5fa94f734
valery.buka <valpal314@gmail.com>
2020-01-10 17:17:58 +0300

    fix content top section styles

--- src/Components/Layout/Content/TopSection/style.js
+++ src/Components/Layout/Content/TopSection/style.js
@@ -2,7 +2,7 @@ import { css } from 'styled-components';
 
 export const TopSectionStyles = css`
     display: flex;
-    padding: 90px 0 120px 0;
+    padding: 90px 100px;
     background-size: 1100px 510px;
     
     @media (max-width: 1023px) {
